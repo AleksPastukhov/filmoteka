b55a306c75ba33cef772e63fa142f5cfd0354bfa
OsokinNikita <110087045+OsokinNikita@users.noreply.github.com>
2023-02-02 16:32:11 +0200

    updete_header
    added: CSS - generic container class.
    js file typed.
    typed is connected.
    made animation on the header

---
+++ src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['Filmoteka'],
+  typeSpeed: 80,
+  showCursor: false,
+  contentType: 'text',
+  loopCount: 5,
+};
+
+var typed = new Typed('.header__logo', options);
