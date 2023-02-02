39604f6945acef46a0fa3a4d73dda52e1e33a792
AleksPastukhov <passandro1990@gmail.com>
2023-02-02 15:21:03 +0200

    add func

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import FilmsApiService from './js/films-service';
 
 const filmServise = new FilmsApiService();
 
-console.log(filmServise.getUrl(trends));
+console.log(filmServise.getUrl('trends'));
+// console.log(filmServise.getFilms('search'));
+
+async function getFilmForUser(type, qv) {
+  return await filmServise.getFilms(type, qv);
+}
+
+getFilmForUser('trends', 'dfg')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
--- src/js/films-service.js
+++ src/js/films-service.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class FilmsApiService {
   #BASE_URL = 'https://api.themoviedb.org/3/';
 
@@ -15,31 +17,35 @@ export default class FilmsApiService {
 
   constructor() {
     this.page = 1;
-    this.userRequest = '';
-    this.type = '';
+    // this.userRequest = '';
+    // this.type = '';
   }
 
-  getUrl() {
-    return `${this.#BASE_URL}${this.type}?api_key=${this.#KEY}&page=${
-      this.page
-    }`;
+  getUrl(type, qv) {
+    return `${this.#BASE_URL}${this.#typeRequest[type]}?api_key=${this.#KEY}`;
+    // return `${this.#BASE_URL}${this.type}?api_key=${this.#KEY}&page=${
+    //   this.page
+    // }`;
   }
 
-  async getFilms() {
-    const axios = require('axios').default;
-    const response = await axios.get(this.getUrl, {
+  async getFilms(type, qv) {
+    const options = {
       params: {
-        page,
-        query: '',
+        page: this.page,
+        query: qv,
         language: 'en-US',
         include_adult: false,
       },
       headers: {
         'Content-Type': 'application/json',
       },
-    });
-    const result = await response.json();
-    console.log(result);
+    };
+
+    // const axios = require('axios').default;
+    const respons = await axios.get(this.getUrl(type), options);
+    return respons.data;
+    // const result = await response.json();
+    // console.log(response);
   }
 
   get userReq() {
@@ -58,6 +64,37 @@ export default class FilmsApiService {
     return (this.type = newTypeReq);
   }
 }
+
+async function lordOfTheRingsAPI(page = 1, type = 'trends', movieId = 758009) {
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+  const TOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTVjMzkyOGJmMjMzNTdlOGE2NzA0NTk3M2M5NTE3OCIsInN1YiI6IjYzZDY0NDY4MjBlNmE1MDBkNTQzZDBjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lD-Jn8MCWel133C-zeEATaTZg8SazetodXbbh1gi0C8';
+  const typeRequest = {
+    search: 'search/movie',
+    video: `movie/${movieId}/videos`,
+    trends: 'trending/movie/week',
+    movieData: `${movieId}`,
+  };
+  const options = {
+    params: {
+      page,
+      query: 'a',
+      language: 'en-US',
+      include_adult: false,
+    },
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const respons = await axios.get(`${BASE_URL}${typeRequest[type]}`, options);
+  return respons.data;
+}
+
+// lordOfTheRingsAPI()
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
 // const axios = require('axios').default;
 
