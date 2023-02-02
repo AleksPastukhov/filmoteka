b8aa951356ecd4070a01b08b17b6fc004dd446e4
Oleksandr FIlippov <filya170895@gmail.com>
2023-02-02 12:59:00 +0100

    added class films-service

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 // import * as myModule from './js';
+import FilmsApiService from './js/films-service';
+
+const filmServise = new FilmsApiService();
+
+console.log(filmServise.getUrl(trends));
--- src/js/films-service.js
+++ src/js/films-service.js
@@ -1,3 +1,64 @@
+export default class FilmsApiService {
+  #BASE_URL = 'https://api.themoviedb.org/3/';
+
+  #TOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTVjMzkyOGJmMjMzNTdlOGE2NzA0NTk3M2M5NTE3OCIsInN1YiI6IjYzZDY0NDY4MjBlNmE1MDBkNTQzZDBjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lD-Jn8MCWel133C-zeEATaTZg8SazetodXbbh1gi0C8';
+
+  #KEY = 'e95c3928bf23357e8a67045973c95178';
+
+  #typeRequest = {
+    search: 'search/movie',
+    // video: `movie/${movieId}/videos`,
+    trends: 'trending/movie/week',
+    // movieData: `${movieId}`,
+  };
+
+  constructor() {
+    this.page = 1;
+    this.userRequest = '';
+    this.type = '';
+  }
+
+  getUrl() {
+    return `${this.#BASE_URL}${this.type}?api_key=${this.#KEY}&page=${
+      this.page
+    }`;
+  }
+
+  async getFilms() {
+    const axios = require('axios').default;
+    const response = await axios.get(this.getUrl, {
+      params: {
+        page,
+        query: '',
+        language: 'en-US',
+        include_adult: false,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const result = await response.json();
+    console.log(result);
+  }
+
+  get userReq() {
+    return this.userRequest;
+  }
+
+  set userReq(newUserReq) {
+    return (this.userRequest = newUserReq);
+  }
+
+  get typeReq() {
+    return this.type;
+  }
+
+  set typeReq(newTypeReq) {
+    return (this.type = newTypeReq);
+  }
+}
+
 // const axios = require('axios').default;
 
 //
@@ -17,16 +78,11 @@
 //   };
 //
 //
-//
-//
-//
-//
 // в конструкторі класу є такі параметри:
 // - сторінка 'page', (номер сторінки запиту)
 // - запит 'userRequest' , (пошуковий запит від клієнта)
 // - тип запиту 'type' (search, video, trends, movieData)
-//
-//
+
 // Даний клас повинен містити такі методи:
 // - створення адреси запиту.
 // - get запит на бекенд (використовуємо axios) приймає typeRequest (передає у метод створення запиту),  з наступними опціями.
@@ -42,8 +98,6 @@
 //       'Content-Type': 'application/json',
 //     },
 //   };
-//
-//
 //
 // - геттер для userRequest
 // - сеттер для userRequest
