3f40c587f873416138b6930557b714b827585d5a
Konstantin2023 <pscosx@gmail.com>
2023-02-04 15:31:44 +0200

    Працюю над кодом!

--- src/js/modal.js
+++ src/js/modal.js
@@ -14,3 +14,19 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+// fetch(
+//   'https://api.themoviedb.org/3/trending/movie/day?api_key=008ad61af513926e956f6d36edd6996c'
+// )
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+// async function getData() {
+//   const response = await fetch(
+//     'https://api.themoviedb.org/3/trending/movie/day?api_key=008ad61af513926e956f6d36edd6996c'
+//   );
+//   return response;
+// }
+// getData()
+//   .then(response => response.json())
+//   .then(data => console.log(data));
