813fb191cabd67517c358bd2229131484ae4a399
AleksPastukhov <109807722+AleksPastukhov@users.noreply.github.com>
2023-02-05 18:39:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
-// import * as myModule from './js';
 import './js/typed';
 import './js/loader';
 import './js/local-storage-info';
 import './js/renderOnSubmit-main-page';
 import './js/render-main-page';
 import './js/roll-up-btn';
-// import FilmsApiService from './js/films-service';
-
-// const filmServise = new FilmsApiService();
-
-// filmServise
-//   .getFilms('trends', 'cat')
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(err => console.log(err));
-
-// filmServise
-//   .getFilmById('video', 76600)
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(err => console.log(err));
 import './js/trailer';
+import './js/modal';
