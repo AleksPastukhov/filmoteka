40c35f3f29fb75192d355d5ab4bb3da47cbade76
BohdanHromchak <gromchakbogdan@gmail.com>
2023-02-05 21:23:40 +0200

    add trailer close-button and functions, correct styles

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,80 +1,44 @@
 import * as basicLightbox from 'basiclightbox';
-// console.log(basicLightbox);
-
-// JS
+import 'remixicon/fonts/remixicon.css';
 import FilmsApiService from './films-service';
 
 const filmServise = new FilmsApiService();
-
 const trailerBtn = document.querySelector('.trailer-btn');
 const filmId = trailerBtn.dataset.id;
 
-trailerBtn.addEventListener('click', onYoutubeBtn);
+trailerBtn.addEventListener('click', onTrailerBtn);
 
-function onYoutubeBtn() {
+function onTrailerBtn() {
   filmServise.getFilmById('video', filmId).then(({ results }) => {
     const trailerKey = results[0].key;
 
-    if (!trailerKey) {
-      return;
+    const instance = basicLightbox.create(` 
+<div class="trailer-wrapper">
+
+  <button class="trailer-btn-close">
+    <i class="ri-close-circle-line"></i>
+  </button>
+
+  <iframe class="trailer-iframe" src="https://www.youtube.com/embed/${trailerKey}">
+  </iframe>
+
+</div>
+`);
+    instance.show();
+
+    const trailerCLosedBtn = document.querySelector('.trailer-btn-close');
+
+    trailerCLosedBtn.addEventListener('click', onCloseBtn);
+    function onCloseBtn() {
+      instance.close();
     }
 
-    const instance = basicLightbox.create(`
-      <iframe
-        class="trailer-iframe"
-        src="https://www.youtube.com/embed/${trailerKey}"
-      >
-      </iframe>
-  
-`);
-
-    instance.show();
+    trailerBtn.addEventListener('keydown', onEscape);
+    function onEscape(evt) {
+      if (evt.code === 'Escape') {
+        instance.close();
+        trailerBtn.removeEventListener('keydown', onEscape);
+      }
+    }
   });
 }
-
-// HTML
-
-// <div class="img-posters">
-//   <img class="img-film container" src="#" alt="#" />
-//   <button class="trailer-btn" data-id="500">
-//     <svg class="trailer-icon" width="80" height="80">
-//       <use href="./images/icons.svg#trailer-btn"></use>
-//     </svg>
-//   </button>
-// </div>;
-
-// SCSS
-
-// .img-posters {
-//   position: relative;
-//   transition: 0.4s;
-// }
-// .trailer-btn {
-//   position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   width: 80px;
-//   height: 80px;
-//   background-color: transparent;
-//   fill: #f79c78;
-//   cursor: pointer;
-//   transition: 0.4s;
-// }
-// .img-posters:hover,
-// .img-posters:focus {
-//   opacity: 0.9;
-//   .trailer-btn {
-//     fill: var(--accent-color);
-//     scale: 1.3;
-//   }
-// }
-// .trailer-icon {
-//   color: currentColor;
-// }
-// .trailer-iframe {
-//   width: 75%;
-//   aspect-ratio: 16 / 9;
-// }
