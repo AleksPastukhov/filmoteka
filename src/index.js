441d3a9d3b363a9905c86641b19adf3eb3abb51e
Иван Чмиль <chmil.ivan.u@gmail.com>
2023-02-06 17:03:15 +0200

    test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './js/local-storage-info';
 import './js/renderOnSubmit-main-page';
 import './js/render-main-page';
 import './js/roll-up-btn';
+import './js/modal-students';
 // import FilmsApiService from './js/films-service';
 
 // const filmServise = new FilmsApiService();
